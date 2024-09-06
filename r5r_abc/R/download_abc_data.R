98781457ee96f976358b6d4f9aa19da723da6f48
Marcus Saraiva <marcus.saraiva@gmail.com>
2024-09-06 10:18:51 -0300

    Download ABC e gitignore

--- r5r_abc/R/download_abc_data.R
+++ r5r_abc/R/download_abc_data.R
@@ -1,7 +1,4 @@
 # Download Open Street Map road network data from GitHub repository
 
 abc_pbf_url <- "https://github.com/jxbarros/15mC_Workshop/releases/download/v1/abc.osm.pbf"
-download.file(abc_pbf_url, destfile = here::here("data_/greater-london-latest.osm.pbf"))
-
-gla_gtfs_url <- "https://data.bus-data.dft.gov.uk/timetable/download/gtfs-file/london/"
-download.file(gla_gtfs_url, destfile = here::here("data_gla/itm_london_gtfs.zip"))
+download.file(abc_pbf_url, destfile = here::here("data/abc.osm.pbf"))
